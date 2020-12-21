9e1a90cf12bd3ec804ee39cadbdc3e23d519ccee
remote-torrent-6747 <abbyc@sky.com>
2020-12-21 15:43:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,8 @@ end
 
 def long_planeteer_calls(calls)
   calls.any? {|word| word.length < 4}
+  else
+    return false  
 end
 #The method should tell us if any of the calls are longer than four characters.
 #should return as true - any?
