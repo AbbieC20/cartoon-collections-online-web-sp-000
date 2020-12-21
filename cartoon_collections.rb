a131ecd460cb02614bcda7d8592c685e07432f37
remote-torrent-6747 <abbyc@sky.com>
2020-12-21 15:20:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   listed_dwarves = []
   dwarves.each_with_index do |dwarf, list|
-    roll_call = "#{list}. *#{dwarf}"
+    roll_call = "#{list + 1}. *#{dwarf}"
     listed_dwarves.push(roll_call)
   end
   return listed_dwarves
